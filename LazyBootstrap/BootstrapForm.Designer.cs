9c910704d773d0f609050b66d1c717e2084e0964
arkitowho <noahdayo20416@gmail.com>
2025-11-10 00:16:19 +0800

    minor text change

--- LazyBootstrap/BootstrapForm.Designer.cs
+++ LazyBootstrap/BootstrapForm.Designer.cs
@@ -68,9 +68,9 @@
             this.groupBoxOptions.Controls.Add(this.chkNoAsphyxia);
             this.groupBoxOptions.Controls.Add(this.chkWindowed);
             this.groupBoxOptions.Controls.Add(this.btnStart);
-            this.groupBoxOptions.Location = new System.Drawing.Point(15, 34);
+            this.groupBoxOptions.Location = new System.Drawing.Point(15, 37);
             this.groupBoxOptions.Name = "groupBoxOptions";
-            this.groupBoxOptions.Size = new System.Drawing.Size(368, 195);
+            this.groupBoxOptions.Size = new System.Drawing.Size(368, 211);
             this.groupBoxOptions.TabIndex = 8;
             this.groupBoxOptions.TabStop = false;
             this.groupBoxOptions.Text = "启动选项";
@@ -78,9 +78,9 @@
             // chkNoRestoreRotation
             // 
             this.chkNoRestoreRotation.AutoSize = true;
-            this.chkNoRestoreRotation.Location = new System.Drawing.Point(163, 167);
+            this.chkNoRestoreRotation.Location = new System.Drawing.Point(163, 181);
             this.chkNoRestoreRotation.Name = "chkNoRestoreRotation";
-            this.chkNoRestoreRotation.Size = new System.Drawing.Size(120, 16);
+            this.chkNoRestoreRotation.Size = new System.Drawing.Size(122, 17);
             this.chkNoRestoreRotation.TabIndex = 7;
             this.chkNoRestoreRotation.Text = "退出时不还原屏幕";
             this.toolTip0.SetToolTip(this.chkNoRestoreRotation, "退出游戏进程时保持当前的旋转方向");
@@ -88,9 +88,9 @@
             // 
             // btnEditConfig
             // 
-            this.btnEditConfig.Location = new System.Drawing.Point(17, 140);
+            this.btnEditConfig.Location = new System.Drawing.Point(17, 152);
             this.btnEditConfig.Name = "btnEditConfig";
-            this.btnEditConfig.Size = new System.Drawing.Size(125, 43);
+            this.btnEditConfig.Size = new System.Drawing.Size(125, 47);
             this.btnEditConfig.TabIndex = 1;
             this.btnEditConfig.Text = "编辑 spicecfg";
             this.toolTip0.SetToolTip(this.btnEditConfig, "如果你勾选了“使用预配置文件”，则会编辑预配置文件");
@@ -99,9 +99,9 @@
             // 
             // btnSwitchRotation
             // 
-            this.btnSwitchRotation.Location = new System.Drawing.Point(295, 137);
+            this.btnSwitchRotation.Location = new System.Drawing.Point(295, 148);
             this.btnSwitchRotation.Name = "btnSwitchRotation";
-            this.btnSwitchRotation.Size = new System.Drawing.Size(54, 23);
+            this.btnSwitchRotation.Size = new System.Drawing.Size(54, 25);
             this.btnSwitchRotation.TabIndex = 6;
             this.btnSwitchRotation.Text = "切换";
             this.btnSwitchRotation.UseVisualStyleBackColor = true;
@@ -111,27 +111,27 @@
             // 
             this.cmbRotation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbRotation.FormattingEnabled = true;
-            this.cmbRotation.Location = new System.Drawing.Point(163, 140);
+            this.cmbRotation.Location = new System.Drawing.Point(163, 152);
             this.cmbRotation.Name = "cmbRotation";
-            this.cmbRotation.Size = new System.Drawing.Size(120, 20);
+            this.cmbRotation.Size = new System.Drawing.Size(120, 21);
             this.cmbRotation.TabIndex = 5;
             this.toolTip0.SetToolTip(this.cmbRotation, "选择你希望屏幕旋转的角度（逆时针）");
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(161, 75);
+            this.label2.Location = new System.Drawing.Point(161, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "调试选项：";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(161, 125);
+            this.label1.Location = new System.Drawing.Point(161, 135);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "屏幕旋转：";
             // 
@@ -140,43 +140,43 @@
             this.chkUsePreconfig.AutoSize = true;
             this.chkUsePreconfig.Checked = true;
             this.chkUsePreconfig.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkUsePreconfig.Location = new System.Drawing.Point(19, 95);
+            this.chkUsePreconfig.Location = new System.Drawing.Point(19, 103);
             this.chkUsePreconfig.Name = "chkUsePreconfig";
-            this.chkUsePreconfig.Size = new System.Drawing.Size(108, 16);
+            this.chkUsePreconfig.Size = new System.Drawing.Size(110, 17);
             this.chkUsePreconfig.TabIndex = 9;
             this.chkUsePreconfig.Text = "使用预配置文件";
-            this.toolTip0.SetToolTip(this.chkUsePreconfig, "启用时将使用预先配置好的最优配置来启动游戏，以防止因错误的配置造成的干扰\n两者互相独立，取消勾选以使用系统内建配置");
+            this.toolTip0.SetToolTip(this.chkUsePreconfig, "使用预先配置好的最优配置文件来启动游戏，以防止因错误的系统内建配置造成的干扰\r\n两者互相独立，取消勾选以使用系统内建配置");
             this.chkUsePreconfig.UseVisualStyleBackColor = true;
             this.chkUsePreconfig.CheckedChanged += new System.EventHandler(this.chkUsePreconfig_CheckedChanged);
             // 
             // chkPCoreOptimization
             // 
             this.chkPCoreOptimization.AutoSize = true;
-            this.chkPCoreOptimization.Location = new System.Drawing.Point(253, 23);
+            this.chkPCoreOptimization.Location = new System.Drawing.Point(253, 25);
             this.chkPCoreOptimization.Name = "chkPCoreOptimization";
-            this.chkPCoreOptimization.Size = new System.Drawing.Size(84, 16);
+            this.chkPCoreOptimization.Size = new System.Drawing.Size(86, 17);
             this.chkPCoreOptimization.TabIndex = 10;
             this.chkPCoreOptimization.Text = "大小核优化";
-            this.toolTip0.SetToolTip(this.chkPCoreOptimization, "勾选此功能后，会将游戏限制在大核上运行");
+            this.toolTip0.SetToolTip(this.chkPCoreOptimization, "勾选后，会限制游戏在性能核心上运行");
             this.chkPCoreOptimization.UseVisualStyleBackColor = true;
             // 
             // chkAsphyxiaDebug
             // 
             this.chkAsphyxiaDebug.AutoSize = true;
-            this.chkAsphyxiaDebug.Location = new System.Drawing.Point(253, 95);
+            this.chkAsphyxiaDebug.Location = new System.Drawing.Point(253, 103);
             this.chkAsphyxiaDebug.Name = "chkAsphyxiaDebug";
-            this.chkAsphyxiaDebug.Size = new System.Drawing.Size(96, 16);
+            this.chkAsphyxiaDebug.Size = new System.Drawing.Size(98, 17);
             this.chkAsphyxiaDebug.TabIndex = 11;
             this.chkAsphyxiaDebug.Text = "调试启动氧无";
-            this.toolTip0.SetToolTip(this.chkAsphyxiaDebug, "以调试模式启动 Asphyxia Core，用于输出错误日志");
+            this.toolTip0.SetToolTip(this.chkAsphyxiaDebug, "以调试模式启动 Asphyxia Core，可输出详细错误日志");
             this.chkAsphyxiaDebug.UseVisualStyleBackColor = true;
             // 
             // chkNetDump
             // 
             this.chkNetDump.AutoSize = true;
-            this.chkNetDump.Location = new System.Drawing.Point(163, 95);
+            this.chkNetDump.Location = new System.Drawing.Point(163, 103);
             this.chkNetDump.Name = "chkNetDump";
-            this.chkNetDump.Size = new System.Drawing.Size(66, 16);
+            this.chkNetDump.Size = new System.Drawing.Size(71, 17);
             this.chkNetDump.TabIndex = 3;
             this.chkNetDump.Text = "NetDump";
             this.chkNetDump.UseVisualStyleBackColor = true;
@@ -184,9 +184,9 @@
             // chkNoAsphyxia
             // 
             this.chkNoAsphyxia.AutoSize = true;
-            this.chkNoAsphyxia.Location = new System.Drawing.Point(163, 45);
+            this.chkNoAsphyxia.Location = new System.Drawing.Point(163, 49);
             this.chkNoAsphyxia.Name = "chkNoAsphyxia";
-            this.chkNoAsphyxia.Size = new System.Drawing.Size(84, 16);
+            this.chkNoAsphyxia.Size = new System.Drawing.Size(86, 17);
             this.chkNoAsphyxia.TabIndex = 2;
             this.chkNoAsphyxia.Text = "不启动氧无";
             this.toolTip0.SetToolTip(this.chkNoAsphyxia, "启动时不启动氧无，连接在线服时可勾选");
@@ -195,9 +195,9 @@
             // chkWindowed
             // 
             this.chkWindowed.AutoSize = true;
-            this.chkWindowed.Location = new System.Drawing.Point(163, 23);
+            this.chkWindowed.Location = new System.Drawing.Point(163, 25);
             this.chkWindowed.Name = "chkWindowed";
-            this.chkWindowed.Size = new System.Drawing.Size(84, 16);
+            this.chkWindowed.Size = new System.Drawing.Size(86, 17);
             this.chkWindowed.TabIndex = 1;
             this.chkWindowed.Text = "窗口化启动";
             this.toolTip0.SetToolTip(this.chkWindowed, "以窗口化模式运行游戏");
@@ -205,21 +205,21 @@
             // 
             // btnStart
             // 
-            this.btnStart.Font = new System.Drawing.Font("宋体", 12F);
-            this.btnStart.Location = new System.Drawing.Point(17, 20);
+            this.btnStart.Font = new System.Drawing.Font("宋体", 10F);
+            this.btnStart.Location = new System.Drawing.Point(17, 22);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(125, 67);
+            this.btnStart.Size = new System.Drawing.Size(125, 73);
             this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "启动";
+            this.btnStart.Text = "启动游戏";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnKillProcesses
             // 
             this.btnKillProcesses.Font = new System.Drawing.Font("宋体", 9F);
-            this.btnKillProcesses.Location = new System.Drawing.Point(19, 93);
+            this.btnKillProcesses.Location = new System.Drawing.Point(19, 98);
             this.btnKillProcesses.Name = "btnKillProcesses";
-            this.btnKillProcesses.Size = new System.Drawing.Size(334, 30);
+            this.btnKillProcesses.Size = new System.Drawing.Size(334, 32);
             this.btnKillProcesses.TabIndex = 8;
             this.btnKillProcesses.Text = "结束所有进程";
             this.toolTip0.SetToolTip(this.btnKillProcesses, "强制结束所有游戏相关进程");
@@ -232,18 +232,18 @@
             this.groupBoxTools.Controls.Add(this.btnInstallRuntime);
             this.groupBoxTools.Controls.Add(this.btnClearCache);
             this.groupBoxTools.Controls.Add(this.btnAddFirewallRule);
-            this.groupBoxTools.Location = new System.Drawing.Point(15, 235);
+            this.groupBoxTools.Location = new System.Drawing.Point(15, 255);
             this.groupBoxTools.Name = "groupBoxTools";
-            this.groupBoxTools.Size = new System.Drawing.Size(368, 140);
+            this.groupBoxTools.Size = new System.Drawing.Size(368, 152);
             this.groupBoxTools.TabIndex = 9;
             this.groupBoxTools.TabStop = false;
             this.groupBoxTools.Text = "工具";
             // 
             // btnInstallRuntime
             // 
-            this.btnInstallRuntime.Location = new System.Drawing.Point(19, 56);
+            this.btnInstallRuntime.Location = new System.Drawing.Point(19, 60);
             this.btnInstallRuntime.Name = "btnInstallRuntime";
-            this.btnInstallRuntime.Size = new System.Drawing.Size(334, 30);
+            this.btnInstallRuntime.Size = new System.Drawing.Size(334, 32);
             this.btnInstallRuntime.TabIndex = 1;
             this.btnInstallRuntime.Text = "安装运行库";
             this.toolTip0.SetToolTip(this.btnInstallRuntime, "安装必要的游戏运行库");
@@ -252,20 +252,20 @@
             // 
             // btnClearCache
             // 
-            this.btnClearCache.Location = new System.Drawing.Point(19, 20);
+            this.btnClearCache.Location = new System.Drawing.Point(19, 22);
             this.btnClearCache.Name = "btnClearCache";
-            this.btnClearCache.Size = new System.Drawing.Size(157, 30);
+            this.btnClearCache.Size = new System.Drawing.Size(157, 32);
             this.btnClearCache.TabIndex = 0;
             this.btnClearCache.Text = "清除 data_mods 缓存";
-            this.toolTip0.SetToolTip(this.btnClearCache, "一键清除data_mods的缓存文件\r\n更新游戏后需清除，以保证歌曲数据库正确读取");
+            this.toolTip0.SetToolTip(this.btnClearCache, "清除data_mods缓存文件\r\n更新游戏后需清除，以重建歌曲类模组数据库");
             this.btnClearCache.UseVisualStyleBackColor = true;
             this.btnClearCache.Click += new System.EventHandler(this.btnClearCache_Click);
             // 
             // btnAddFirewallRule
             // 
-            this.btnAddFirewallRule.Location = new System.Drawing.Point(192, 20);
+            this.btnAddFirewallRule.Location = new System.Drawing.Point(192, 22);
             this.btnAddFirewallRule.Name = "btnAddFirewallRule";
-            this.btnAddFirewallRule.Size = new System.Drawing.Size(161, 30);
+            this.btnAddFirewallRule.Size = new System.Drawing.Size(161, 32);
             this.btnAddFirewallRule.TabIndex = 2;
             this.btnAddFirewallRule.Text = "添加防火墙规则";
             this.toolTip0.SetToolTip(this.btnAddFirewallRule, "添加允许游戏通过防火墙的规则");
@@ -275,9 +275,9 @@
             // lblLogOutput
             // 
             this.lblLogOutput.AutoSize = true;
-            this.lblLogOutput.Location = new System.Drawing.Point(392, 34);
+            this.lblLogOutput.Location = new System.Drawing.Point(392, 37);
             this.lblLogOutput.Name = "lblLogOutput";
-            this.lblLogOutput.Size = new System.Drawing.Size(65, 12);
+            this.lblLogOutput.Size = new System.Drawing.Size(67, 13);
             this.lblLogOutput.TabIndex = 11;
             this.lblLogOutput.Text = "日志输出：";
             // 
@@ -286,12 +286,11 @@
             this.txtLogOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtLogOutput.Location = new System.Drawing.Point(394, 54);
-            this.txtLogOutput.Multiline = true;
+            this.txtLogOutput.Location = new System.Drawing.Point(394, 59);
             this.txtLogOutput.Name = "txtLogOutput";
             this.txtLogOutput.ReadOnly = true;
             this.txtLogOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.txtLogOutput.Size = new System.Drawing.Size(378, 415);
+            this.txtLogOutput.Size = new System.Drawing.Size(378, 449);
             this.txtLogOutput.TabIndex = 12;
             this.txtLogOutput.Text = "";
             // 
@@ -300,30 +299,30 @@
             this.groupBoxCompatLayer.Controls.Add(this.lblCompatStatus);
             this.groupBoxCompatLayer.Controls.Add(this.btnUnloadCompat);
             this.groupBoxCompatLayer.Controls.Add(this.button1);
-            this.groupBoxCompatLayer.Location = new System.Drawing.Point(15, 381);
+            this.groupBoxCompatLayer.Location = new System.Drawing.Point(15, 413);
             this.groupBoxCompatLayer.Name = "groupBoxCompatLayer";
-            this.groupBoxCompatLayer.Size = new System.Drawing.Size(368, 90);
+            this.groupBoxCompatLayer.Size = new System.Drawing.Size(368, 98);
             this.groupBoxCompatLayer.TabIndex = 10;
             this.groupBoxCompatLayer.TabStop = false;
             this.groupBoxCompatLayer.Text = "AMD/Intel显卡兼容层";
-            this.toolTip0.SetToolTip(this.groupBoxCompatLayer, "载入AMD/Intel显卡的兼容层使其正确运行");
+            this.toolTip0.SetToolTip(this.groupBoxCompatLayer, "载入AMD/Intel显卡的兼容层");
             // 
             // lblCompatStatus
             // 
             this.lblCompatStatus.AutoSize = true;
             this.lblCompatStatus.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold);
             this.lblCompatStatus.ForeColor = System.Drawing.Color.Red;
-            this.lblCompatStatus.Location = new System.Drawing.Point(17, 65);
+            this.lblCompatStatus.Location = new System.Drawing.Point(17, 70);
             this.lblCompatStatus.Name = "lblCompatStatus";
             this.lblCompatStatus.Size = new System.Drawing.Size(64, 12);
             this.lblCompatStatus.TabIndex = 2;
-            this.lblCompatStatus.Text = "● 未载入";
+            this.lblCompatStatus.Text = "● 未启用";
             // 
             // btnUnloadCompat
             // 
-            this.btnUnloadCompat.Location = new System.Drawing.Point(192, 20);
+            this.btnUnloadCompat.Location = new System.Drawing.Point(192, 22);
             this.btnUnloadCompat.Name = "btnUnloadCompat";
-            this.btnUnloadCompat.Size = new System.Drawing.Size(157, 33);
+            this.btnUnloadCompat.Size = new System.Drawing.Size(157, 36);
             this.btnUnloadCompat.TabIndex = 1;
             this.btnUnloadCompat.Text = "关闭";
             this.btnUnloadCompat.UseVisualStyleBackColor = true;
@@ -331,9 +330,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(19, 20);
+            this.button1.Location = new System.Drawing.Point(19, 22);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(157, 33);
+            this.button1.Size = new System.Drawing.Size(157, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "启用";
             this.button1.UseVisualStyleBackColor = true;
@@ -343,7 +342,7 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 481);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 523);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(784, 22);
             this.statusStrip1.SizingGrip = false;
@@ -358,9 +357,9 @@
             // 
             // BootstrapForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 503);
+            this.ClientSize = new System.Drawing.Size(784, 545);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBoxCompatLayer);
             this.Controls.Add(this.txtLogOutput);
