50799803244f575e3f9173d02657c327fc7fcf0c
arkitowho <noahdayo20416@gmail.com>
2025-12-18 14:05:46 +0800

    proj info fix

--- Clean-Build.bat
+++ Clean-Build.bat
@@ -3,7 +3,7 @@ setlocal EnableExtensions
 
 rem Clean build caches (bin and obj) only inside the project folder
 set "ROOT=%~dp0"
-set "PROJECT_DIR=%ROOT%LazyBootstrap\LazyBootstrap"
+set "PROJECT_DIR=%ROOT%LazyBootstrap"
 
 if not exist "%PROJECT_DIR%" (
   echo Project directory not found: "%PROJECT_DIR%"
@@ -15,20 +15,10 @@ echo Cleaning build caches under "%PROJECT_DIR%" ...
 
 set "COUNT=0"
 
-for /d /r "%PROJECT_DIR%" %%d in (bin) do (
-  if /i "%%~nxd"=="bin" (
-    echo Deleting "%%d"
-    rmdir /s /q "%%d" 2>nul
-    set /a COUNT+=1 >nul 2>&1
-  )
-)
-
-for /d /r "%PROJECT_DIR%" %%d in (obj) do (
-  if /i "%%~nxd"=="obj" (
-    echo Deleting "%%d"
-    rmdir /s /q "%%d" 2>nul
-    set /a COUNT+=1 >nul 2>&1
-  )
+for /d /r "%PROJECT_DIR%" %%d in (bin,obj) do (
+  echo Deleting "%%d"
+  rmdir /s /q "%%d" 2>nul
+  set /a COUNT+=1 >nul 2>&1
 )
 
 echo.
